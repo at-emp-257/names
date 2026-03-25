92f90f16e6d94505d9a6e076f10db4b9425ec0d8
Md Sujauddin Sekh <sujauddin@trackier.com>
2026-03-25 18:44:22 +0530

    v1.1.0

---
+++ colors/colors.go
@@ -0,0 +1,15 @@
+package colors
+
+import (
+	"github.com/at-emp-257/names/utils"
+)
+
+var colors = []string{
+	"aqua",
+	"black",
+	"cyan",
+}
+
+func PrintAll() {
+	utils.PrintAll("Printing colors", colors)
+}
